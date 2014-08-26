f2c25a4572f729aa2401c1958a4f857aa2e37a5d
cfc <cfc@ufrj.br>
2014-08-26 15:19:23 -0300

    import inicial

---
+++ SistemaVendas/src/br/com/k21/CalculadoraComissao.java
@@ -0,0 +1,17 @@
+package br.com.k21;
+
+public class CalculadoraComissao {
+
+	public static Double calcula(Double valorVenda) {
+		
+		double comissao = valorVenda*5/100;
+		comissao = trataArredondamento(comissao);
+		return comissao;
+
+	}
+
+	private static double trataArredondamento(double comissao) {
+		return Math.floor((comissao)*100)/100;
+	}
+	
+}
---
+++ SistemaVendas/testes/br/com/k21/TestCalculadoraComissao.java
@@ -0,0 +1,48 @@
+package br.com.k21;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestCalculadoraComissao {
+	
+	@Test
+	public void testeValorVendaMenorQue10000Valor100Comissao5() {
+		Double valorVenda = 100.0;
+		Double esperado = 5.0;
+		
+		Double comissao = CalculadoraComissao.calcula(valorVenda);
+		
+		assertEquals(esperado, comissao);
+	}
+	
+	@Test
+	public void testeValorVendaMenorQue10000Valor10000Comissao500() {
+		Double valorVenda = 10000.0;
+		Double esperado = 500.0;
+		
+		Double comissao = CalculadoraComissao.calcula(valorVenda);
+		
+		assertEquals(esperado, comissao);
+	}
+	
+	@Test
+	public void testeValorVendaMenorQue10000Valor1RealComissao5Centavos() {
+		Double valorVenda = 1.0;
+		Double esperado = 0.05;
+		
+		Double comissao = CalculadoraComissao.calcula(valorVenda);
+		
+		assertEquals(esperado, comissao);
+	}
+	
+	@Test
+	public void testeValorVendaMaiorQue10000Valor55Reais59CentavosComissao2Reais77Centavos() {
+		Double valorVenda = 55.59;
+		Double esperado = 2.77;
+		
+		Double comissao = CalculadoraComissao.calcula(valorVenda);
+		
+		assertEquals(esperado, comissao);
+	}
+
+}
